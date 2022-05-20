d1432f88792f72ee836fcbac6b6c658ff5de29bf
Xavier M <golf4Xavier@gmail.com>
2022-05-20 10:15:16 -0400

    adding vs file

---
+++ switch-looops/switch-looops/Program.cs
@@ -0,0 +1,24 @@
+﻿// switch
+
+Console.WriteLine("Please give me a number 1-3");
+string userinput = Console.ReadLine();
+switch(userinput)
+{
+    case "1":
+        Console.WriteLine("you choice the number 1");
+        break;
+    case "2":
+        Console.WriteLine("you choice the number 2");
+        break;
+    case "3":
+        Console.WriteLine("you choice the number 3");
+        break;
+    default:
+        Console.WriteLine("Please choose a number between number 1-3");
+        break;
+}
+//cw + tab tab == this
+Console.WriteLine();
+// loops
+
+//while
